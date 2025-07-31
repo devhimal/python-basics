a03d67d3e78c14dbefabf361e703811a1c3b266f
devhimal <himalhobbs.364@gmail.com>
2025-07-31 10:42:12 +0545

    feat: adding new exercise of functions

--- class-task/functions.py
+++ class-task/functions.py
@@ -49,3 +49,59 @@ def print_args(*args, **kwargs):
     print("Keyword arguments:", kwargs)
 
 print_args(1, 2, 3, name="Alice", age=30)
+
+# Create a dictionary and write a function to search a valueusing key and return the specific value.
+
+
+students = {
+    1: {"name": "Alice", "age": 20, "grade": "A"},
+    2: {"name": "Bob", "age": 21, "grade": "B"},
+    3: {"name": "Charlie", "age": 22, "grade": "A"},
+    4: {"name": "David", "age": 20, "grade": "C"},
+    5: {"name": "Eve", "age": 23, "grade": "B"},
+    6: {"name": "Frank", "age": 19, "grade": "A"},
+    7: {"name": "Grace", "age": 22, "grade": "B"},
+    8: {"name": "Heidi", "age": 21, "grade": "C"},
+    9: {"name": "Ivan", "age": 20, "grade": "B"},
+    10: {"name": "Judy", "age": 23, "grade": "A"}
+}
+
+# Example: print details of student with ID 3
+print(students[3])
+
+def find_student_by_id(student_id):
+    if student_id in students:
+        return students[student_id]
+    else:
+        return "Student not found."
+
+finded_student = find_student_by_id(10)
+
+print(f"Name of student with ID 10 Name: {finded_student['name']}")
+print(f"Name of student with ID 10 Age: {finded_student['age']}")
+print(f"Name of student with ID 10 Grade: {finded_student['grade']}")
+print(f"Details of student with ID 11: {find_student_by_id(11)}") # This will return "Student not found."
+
+
+
+#Function Decorators, this allow us to modify and extend the behaviour  of functions without changing the code.
+
+
+import time
+
+def timer_decorator(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"Function '{func.__name__}' took {end_time - start_time:.4f} seconds to execute.")
+        return result
+    return wrapper
+
+@timer_decorator
+def example_function():
+    time.sleep(2)
+    print("Function executed.")
+
+example_function()
+
